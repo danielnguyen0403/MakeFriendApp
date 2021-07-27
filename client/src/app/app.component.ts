320fa862ba90c2df65cb07c0156b66b969a83cc1
Hao Nguyen <danielnguyen0403@gmail.com>
2021-07-27 10:42:04 -0500

    routing, theme, route guard, share module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'Make Friend App';
-  users: any;
+  users: any;   
 
   constructor(private accountService: AccountService) {}
   
